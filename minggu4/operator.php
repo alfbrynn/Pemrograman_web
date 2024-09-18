d89668266c67d526726e0730c0f4d6baed792a9c
alfbrynn <aliffebriansyah@gmail.com>
2024-09-18 16:25:09 +0700

    update

---
+++ minggu4/operator.php
@@ -0,0 +1,35 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+// Pertanyaan 9
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+// pertanyaan 10
+$hasilAnd = $a && $b;
+$hasilOr = $a || $b;
+$hasilNotA = !$a;
+$hasilNotB = !$b;
+
+// Pertanyaan 11
+$a += $b;
+$a -= $b;
+$a *= $b;
+$a /= $b;
+$a %= $b;
+
+// pertanyaan 12
+$hasilIdentik = $a === $b;
+$hasilTidakIdentik = $a !== $b;
--- minggu4/strukturkontrol.php
+++ minggu4/strukturkontrol.php
@@ -21,4 +21,37 @@ while ($jarakSaatIni < $jarakTarget) {
     $jarakSaatIni += $peningkatanHarian;
     $hari++;
 }
+
+// pertanyaan 16
+$jumlahLahan = 10;
+$tanamanPerlahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+for ($i = 1, $i <= $jumlahLahan; $i++) {
+    $jumlahBuah += ($tanamanPerlahan * $buahPerTanaman);
+}
+
+echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
+
+// pertanyaan 17
+$skorUjian = [85, 92, 78, 96, 88];
+$totalSkor = 0;
+
+foreach ($skorUjian as $skor) {
+    $totalSkor += $skor;
+}
+
+echo "Total skor ujian: $totalSkor";
+
+// pertanyaan 18
+$nilaiSiswa = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+
+foreach ($nilaiSiswa as $nilai) {
+    if ($nilai < 60){
+        echo "Nilai: $nilai (tidak lulus) <br>";
+        continue;
+    }
+    echo "Nilai: $nilai (lulus) <br>";
+}
 ?>
