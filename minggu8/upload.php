21f38c0615ecc73c1714afb47b944f19f1469e85
alfbrynn <aliffebriansyah@gmail.com>
2024-10-22 21:24:26 +0700

    update

--- minggu8/upload.php
+++ minggu8/upload.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 if (isset($_POST["submit"])) {
     $targetdir = "uploads/"; //Direktori tujuan untuk menyimpan file
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
@@ -9,45 +9,35 @@ if (isset($_POST["submit"])) {
         echo "file gagal diunggah";
     }
 }
-?>
-
-<!-- <?php
-if (isset($_POST["submit"])) {
-    $targetdir = "uploads/";
-    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
-
-    if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-        echo "file berhasil diunggah";
-    } else {
-        echo "file gagal diunggah";
-    }
-}
 ?> -->
 
 <!-- <?php
-if(isset($_POST["submit"])){
+if (isset($_POST["submit"])) {
     $target_dir = "uploads/"; // Direktori tujuan untuk menyimpan file
     $target_file = $target_dir . basename($_FILES["myfile"]["name"]);
-    $file_type = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    $file_type = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
     // Ekstensi file yang diizinkan
     $allowed_extensions = array("jpg", "jpeg", "png", "gif");
     // Ukuran maksimum file (5MB)
     $max_size = 5 * 1024 * 1024;
 
-    if(in_array($file_type, $allowed_extensions) && $_FILES["myfile"]["size"] <= $max_size){
-        if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $target_file)){
-            echo "File berhasil diunggah.";
+    if (in_array($file_type, $allowed_extensions) && $_FILES["myfile"]["size"] <= $max_size) {
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $target_file)) {
+            echo "File berhasil diunggah.<br>";
+            // Menampilkan thumbnail gambar setelah berhasil diunggah
+            echo "<img src='" . $target_file . "' width='200' style='height: auto;' />";
         } else {
             echo "Gagal mengunggah file.";
         }
     } else {
-        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
     }
 }
 ?> -->
 
-<!-- <?php
+
+<?php
 if(isset($_POST["submit"])){
     $target_dir = "uploads/"; // Direktori tujuan untuk menyimpan file
     $target_file = $target_dir . basename($_FILES["myfile"]["name"]);
@@ -56,7 +46,7 @@ if(isset($_POST["submit"])){
     // Ekstensi file yang diizinkan
     $allowed_extensions = array("txt", "pdf", "doc", "dox");
     // Ukuran maksimum file (5MB)
-    $max_size = 3 * 1024 * 1024;
+    $max_size = 1 * 1024 * 1024;
 
     if(in_array($file_type, $allowed_extensions) && $_FILES["myfile"]["size"] <= $max_size){
         if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $target_file)){
@@ -68,4 +58,4 @@ if(isset($_POST["submit"])){
         echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
     }
 }
-?> -->
+?>
